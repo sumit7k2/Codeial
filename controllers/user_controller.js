34038bb54be8f10f91567460f143910d62f76b92
Sumit Kumar <sumit7k2.sk@gmail.com>
2021-04-24 20:19:11 +0530

    add sign-up and sign-in forms - ejs files to view folder

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,3 +4,27 @@ module.exports.profile=function(req,res){
         name:'Sumit'
     });
 }
+
+// Render signup page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:'Sign up'
+    });
+}
+
+// render signin page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:'Sign in'
+    });
+}
+
+// Getting data from signup form
+module.exports.create=function(req,res){
+    // todo later
+}
+
+// sign-in and create session for the user
+module.exports.createSession=function(req,res){
+    // todo later
+}
