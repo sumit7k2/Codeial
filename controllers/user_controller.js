ff8c533802ea90c85388adc0831052cd353801cc
Sumit Kumar <sumit7k2.sk@gmail.com>
2021-04-24 23:14:45 +0530

    implement signup in controller and router part

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
     return res.render('user',{
         title:'User',
@@ -21,7 +23,22 @@ module.exports.signIn=function(req,res){
 
 // Getting data from signup form
 module.exports.create=function(req,res){
-    // todo later
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing up'); return;}
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error in creating user while signing up'); return;}
+
+                return res.redirect('/user/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 
 // sign-in and create session for the user
--- route/users.js
+++ route/users.js
@@ -5,7 +5,8 @@ const userController=require('../controllers/user_controller');
 
 router.get('/profile',userController.profile);
 router.get('/sign-in',userController.signIn);
-router.get('/sign-up',userController.signUp)
+router.get('/sign-up',userController.signUp);
+ router.post('/create',userController.create);
 
 
 module.exports=router;
